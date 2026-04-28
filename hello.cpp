7683bd5bc0b2faf1533dc59e69281a6ca324004e
Kiril Kolga <kirill.kolga@gmail.com>
2026-04-28 17:13:06 +0300

    added function to ask user their name

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <string>
 
-std::string name = "Kirppari";
+std::string askForName()
+{
+    std::string name;
+
+    while (true)
+    {
+        std::cout << "Add your name: ";
+        std::cin >> name;
+
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            continue;
+        }
+        break;
+    }
+
+    return name;
+}
 
 int main()
 {
+    std::string name = askForName();
+
     std::cout << "Hello " + name + "!";
     return 0;
 }
